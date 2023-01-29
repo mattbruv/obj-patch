095b3021a647d05be63a4ef1d72159ddf9aa79c8
Matt P <5638426+mattbruv@users.noreply.github.com>
2023-01-28 20:21:21 -0400

    initial commit

---
+++ ObjPatch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using LibObjectFile.Elf;
+using CommandLine;
+
+namespace ObjPatch
+{
+    internal class Program
+    {
+        public class Options
+        {
+            [Option('i', "input-obj", Required = true)]
+            public string? InputObjPath { get; set; }
+
+            [Option('p', "patch-obj", Required = true)]
+            public string? PatchObjPath { get; set; }
+
+            [Option('o', "out", Required = false, Default = "out.o")]
+            public string? OutObjPath { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            CommandLine.Parser.Default.ParseArguments<Options>(args).WithParsed(Run);
+        }
+
+        static void Run(Options opts)
+        {
+            Console.WriteLine(opts.InputObjPath);
+            Console.WriteLine(opts.OutObjPath);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# ObjPatch
+
+A command line tool to patch an object file,
+Created for the BFBB decompilation project.
